37ad07d02120e62dfa913ea66158506ad53bdbcb
afk <afk@ellugar.co>
2018-08-25 04:20:31 -0500

    migrate to django 2.0

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,18 @@
-from django.db import models
-from cms.models import Therapist
-from taggit.managers import TaggableManager
 from ckeditor.fields import RichTextField
+from cms.models import Therapist
+from django.db import models
+from taggit.managers import TaggableManager
 from uuslug import uuslug
+
 
 def upload_to_entry(instance, filename):
     import os.path
     filename_base, filename_ext = os.path.splitext(filename)
     return 'blog/entry/%s%s' % (
         instance.slug,
-        filename_ext.lower(),)
+        filename_ext.lower(),
+    )
+
 
 class Entry(models.Model):
     title = models.CharField(max_length=100, unique=True)
@@ -18,13 +21,14 @@ class Entry(models.Model):
     text = RichTextField()
     social_description = models.CharField(max_length=140)
     tags = TaggableManager()
-    author = models.ForeignKey(Therapist)
+    author = models.ForeignKey(Therapist, on_delete=models.CASCADE)
     image = models.FileField(upload_to=upload_to_entry, blank=True, null=True)
     dateCreated = models.DateField(auto_now_add=True, editable=False)
     dateUpdated = models.DateField(auto_now=True, editable=False)
-    @models.permalink
+
     def get_absolute_url(self):
-        return ('entry-detail', None, { 'slug': self.slug })
+        return reverse('entry-detail', None, {'slug': self.slug})
+
     def save(self, *args, **kwargs):
         self.slug = uuslug(self.title, instance=self, slug_field='slug')
         if self.pk is None:
@@ -33,8 +37,10 @@ class Entry(models.Model):
             super(Entry, self).save(*args, **kwargs)
             self.image = saved_image
         super(Entry, self).save(*args, **kwargs)
+
     def __str__(self):
         return self.title
+
     class Meta:
         verbose_name = 'Blog Entry'
         verbose_name_plural = 'Blog Entries'
